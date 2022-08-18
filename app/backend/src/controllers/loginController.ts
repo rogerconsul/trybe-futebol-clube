21a1226930bb794cf0ce89229b7ae19df1c83077
Roger Consul <rogerconsul@yahoo.com.br>
2022-08-18 16:42:53 -0300

    Implementado regex pra validar email

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from 'express';
 import userService from '../services/userService';
 
+const regex = /\S+@\S+\.\S+/;
+
 const loginController = {
   async loginUser(req: Request, res: Response) {
     const { email, password } = req.body;
     console.log(email, password);
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    if (!regex.test(email)) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
     }
     const result = await userService.loginUser(email);
     res.status(result.status).json(result.message);
