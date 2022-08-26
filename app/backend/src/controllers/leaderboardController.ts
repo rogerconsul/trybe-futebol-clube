fb7590af9cb7b432c3bed13b0d8c8b8ec97a7012
Roger Consul <rogerconsul@yahoo.com.br>
2022-08-26 11:11:15 -0300

    Req 29 e o surfista prateado

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -16,6 +16,7 @@ let temp = {
   goalsBalance: 0,
   efficiency: '0',
 };
+
 let leaderBoard: tempObject[] = [];
 
 const usingWhoWon = async (gp: number, gc: number) => {
@@ -54,17 +55,6 @@ const reset = () => {
   };
 };
 
-// const organizador = (array: tempObject[]) => {
-//   const first = array.sort((a: tempObject, b: tempObject) => {
-//     (a.totalPoints < b.totalPoints) ? 1 : -1;
-//     (a.totalVictories < b.totalVictories) ? 1 : -1;
-//     (a.goalsBalance < b.goalsBalance) ? 1 : -1;
-//     (a.goalsFavor < b.goalsFavor) ? 1 : -1;
-//     (a.goalsOwn < b.goalsOwn) ? 1 : -1;
-//     return 0;
-//   });
-//   return first;
-// };
 const organizador = (array: tempObject[]) => array.sort(
   (a, b) =>
     b.totalPoints - a.totalPoints
@@ -77,17 +67,19 @@ const organizador = (array: tempObject[]) => array.sort(
 const leaderboardController = {
   async generateLeaderboardHome(_req: Request, res: Response) {
     const teams = await Team.findAll();
-    teams.forEach(async (team) => {
+    const bigTask = teams.map(async (team) => {
       const matchesTheTeamPlayedAsHome = await matchService.getByHomeTeamId(team.id);
       temp.name = team.teamName;
       temp.totalGames = matchesTheTeamPlayedAsHome.length;
-      matchesTheTeamPlayedAsHome.forEach(async (match) => {
+      const tarefas = matchesTheTeamPlayedAsHome.map(async (match) => {
         await usingWhoWon(match.homeTeamGoals, match.awayTeamGoals);
       });
+      await Promise.all(tarefas);
       temp.efficiency = aproveitamento(temp.totalPoints, temp.totalGames).toFixed(2);
       leaderBoard.push(temp);
       reset();
     });
+    await Promise.all(bigTask);
     const sort = organizador(leaderBoard);
     res.status(200).json(sort);
     leaderBoard = [];
